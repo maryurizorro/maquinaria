691123766f74ab4ef6e0f9a6f16112dcf3e445d4
maryurizorro <maryurilizethzorrosilva5@gmail.com>
2025-09-14 15:57:42 -0500

    .

---
+++ maquinaria_laravel/database/migrations/2025_09_12_211153_add_campos_to_mantenimientos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mantenimientos', function (Blueprint $table) {
+            $table->integer('tiempo_estimado')->after('costo')->comment('Tiempo estimado en horas');
+            $table->text('manual_procedimiento')->after('tiempo_estimado');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mantenimientos', function (Blueprint $table) {
+            $table->dropColumn(['tiempo_estimado', 'manual_procedimiento']);
+        });
+    }
+};
